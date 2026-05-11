52a5f236c1622c71c78af8a653a68ec1a3a3db5a
Krishnan Thampi <nkrishnanthampi@gmail.com>
2026-05-11 17:06:57 +0100

    Working version of Airport Finder v02.1

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,28 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata = {
-  title: "Airlines from UK airports",
-  description: "Find which airlines fly from any UK airport.",
+  title: "Airport Finder",
+  description: "Find airlines and destinations from UK airports",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
